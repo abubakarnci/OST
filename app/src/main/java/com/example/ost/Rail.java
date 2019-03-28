9395ab04f1f564a493b72b8a80f2b65a15c3fd68
Abubakar <abubakargill538@gmail.com>
2019-03-28 13:30:41 +0000

    Abubakar

--- app/src/main/java/com/example/ost/Rail.java
+++ app/src/main/java/com/example/ost/Rail.java
@@ -9,5 +9,6 @@ public class Rail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rail);
+        //Hellow
     }
 }
