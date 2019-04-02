c2f78b79df7a8952300a65d6ef53ba401a3c2a19
Umer Iqbal <umer.iqbal01@gmail.com>
2019-04-02 16:55:11 +0100

    Updated

--- app/src/main/java/com/example/ost/Bus.java
+++ app/src/main/java/com/example/ost/Bus.java
@@ -10,6 +10,7 @@ public class Bus extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus);
+        //Hi
 
 
 
