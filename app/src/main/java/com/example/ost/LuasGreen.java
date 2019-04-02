36cba70dd29a0b92e7e5c52977b08b7fb3126b94
Jay <jay.smith.college@gmail.com>
2019-04-02 11:09:13 +0100

    test

---
+++ app/src/main/java/com/example/ost/LuasGreen.java
@@ -0,0 +1,21 @@
+package com.example.ost;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.EditText;
+
+public class LuasGreen extends Activity {
+
+    public void greenSearchBtn(View view){
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luas_green);
+    }
+
+}
