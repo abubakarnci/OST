922198f1618c87bf01114f9f4f17e7d5223cd1d7
Abubakar <abubakargill538@gmail.com>
2019-04-05 22:40:19 +0100

    Abubakar

--- app/src/main/java/com/example/ost/LuasGreen.java
+++ app/src/main/java/com/example/ost/LuasGreen.java
@@ -31,7 +31,6 @@ public class LuasGreen extends AppCompatActivity implements AdapterView.OnItemSe
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-        //String input="";
         String text= parent.getItemAtPosition(position).toString();
         Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
 
@@ -45,6 +44,42 @@ public class LuasGreen extends AppCompatActivity implements AdapterView.OnItemSe
         Uri Cabra = Uri.parse(tfi+"70");
         Intent gotoCabra = new Intent(Intent.ACTION_VIEW, Cabra);
 
+        //Grange
+        Uri Grange = Uri.parse(tfi+"68");
+        Intent gotoGrange = new Intent(Intent.ACTION_VIEW, Grange);
+
+        //Dominick
+        Uri Dominick = Uri.parse(tfi+"66");
+        Intent gotoDominick = new Intent(Intent.ACTION_VIEW, Dominick);
+
+        //Parnell
+        Uri Parnell = Uri.parse(tfi+"65");
+        Intent gotoParnell = new Intent(Intent.ACTION_VIEW, Parnell);
+
+        //Phibsborough
+        Uri Phibsborough = Uri.parse(tfi+"69");
+        Intent gotoPhibsborough = new Intent(Intent.ACTION_VIEW, Phibsborough);
+
+        //DIT
+        Uri DIT = Uri.parse(tfi+"67");
+        Intent gotoDIT = new Intent(Intent.ACTION_VIEW, DIT);
+
+        //Marlborough
+        Uri Marlborough = Uri.parse(tfi+"62");
+        Intent gotoMarlborough = new Intent(Intent.ACTION_VIEW, Marlborough);
+
+        //Trinity
+        Uri Trinity = Uri.parse(tfi+"60");
+        Intent gotoTrinity = new Intent(Intent.ACTION_VIEW, Trinity);
+
+        //Dawson
+        Uri Dawson = Uri.parse(tfi+"59");
+        Intent gotoDawson = new Intent(Intent.ACTION_VIEW, Dawson);
+
+        //Harcourt
+        Uri Harcourt = Uri.parse(tfi+"25");
+        Intent gotoHarcourt = new Intent(Intent.ACTION_VIEW, Harcourt);
+
 
         if (gotoBroombridge.resolveActivity(getPackageManager()) != null) {
 
@@ -54,6 +89,42 @@ public class LuasGreen extends AppCompatActivity implements AdapterView.OnItemSe
             }
             else if(text.equals("Cabra")){
                 startActivity(gotoCabra);
+
+            }
+            else if(text.equals("Grange")){
+                startActivity(gotoGrange);
+
+            }
+            else if(text.equals("Dominick")){
+                startActivity(gotoDominick);
+
+            }
+            else if(text.equals("Parnell")){
+                startActivity(gotoParnell);
+
+            }
+            else if(text.equals("Phibsborough")){
+                startActivity(gotoPhibsborough);
+
+            }
+            else if(text.equals("Marlborough")){
+                startActivity(gotoMarlborough);
+
+            }
+            else if(text.equals("DIT")){
+                startActivity(gotoDIT);
+
+            }
+            else if(text.equals("Trinity")){
+                startActivity(gotoTrinity);
+
+            }
+            else if(text.equals("Harcourt")){
+                startActivity(gotoHarcourt);
+
+            }
+            else if(text.equals("Dawson")){
+                startActivity(gotoDawson);
 
             }
         }
